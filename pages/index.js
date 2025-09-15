d46003ae54d32bd4a221466c774ab93b33f030ae
Steve Aceto <ronh591@gmail.com>
2025-09-14 19:33:58 -0700

    added custom css styles to utils.module and commented js in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,28 @@
+// Import the Head component from Next.js for modifying the <head> of the page
 import Head from 'next/head';
+
+// Import the Layout component and siteTitle constant from local layout component
 import Layout, { siteTitle } from '../components/layout';
+
+// Import CSS module with utility styles
 import utilStyles from '../styles/utils.module.css';
+
+// Import function to get sorted post data from local posts library
 import { getSortedPostsData } from '../lib/posts';
+
+// Import Link component from Next.js for client-side navigation
 import Link from 'next/link';
+
+// Import Date component for formatting date strings
 import Date from '../components/date';
 
+// Default export of the Home page component
+// Receives allPostsData as props (fetched by getStaticProps)
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
-        <title>{siteTitle}</title>
+        <title>{siteTitle}</title> {/* Set the page title dynamically */}
       </Head>
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
@@ -23,10 +36,10 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
+              <Link href={`/posts/${id}`}>{title}</Link> {/* Link to individual post */}
               <br />
               <small className={utilStyles.lightText}>
-                <Date dateString={date} />
+                <Date dateString={date} /> {/* Render formatted post date */}
               </small>
             </li>
           ))}
@@ -36,11 +49,16 @@ export default function Home({ allPostsData }) {
   );
 }
 
+// Next.js function to fetch data at build time (Static Generation)
+// Runs on the server side during the build
 export async function getStaticProps() {
+  // Call local function to get all posts sorted by date
   const allPostsData = getSortedPostsData();
+
+  // Return the data as props to the Home component
   return {
     props: {
-      allPostsData,
+      allPostsData, // will be passed to Home() as a prop
     },
   };
 }
