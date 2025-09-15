78decde13a521eb7051dd7b352ab0f8d8b378a9d
Steve Aceto <ronh591@gmail.com>
2025-09-14 17:38:44 -0700

    added date.js file, updated lib/posts.js and id.js in pages and posts respositories

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,41 @@
 import Head from 'next/head';
-// Import Layout component and siteTitle constant from layout.js
 import Layout, { siteTitle } from '../components/layout';
-// Import shared utility CSS module for styling
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
 
-// Fetch blog posts at build time
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this in{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
   return {
@@ -13,38 +43,4 @@ export async function getStaticProps() {
       allPostsData,
     },
   };
-}
-
-// Define and export the Home page component
-export default function Home({ allPostsData }) {
-  return (
-    <Layout home>
-      <Head>
-        {/* Set the page title using the siteTitle constant */}
-        <title>{siteTitle}</title>
-      </Head>
-
-      {/* Self-introduction section */}
-      <section className={utilStyles.headingMd}>
-        <p className={utilStyles.intro}>[Hello, my name is Steve A]</p>
-        <p className={utilStyles.description}>
-          (This is the basis of the website I will build in CS55{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-
-      {/* Blog posts section */}
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <h3>{title}</h3>
-              <small>{id} — {date}</small>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
-  );
 }
